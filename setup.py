67abc9d8edd3b094f8d3d9d19eae016730762559
Rollin Thomas <rcthomas@lbl.gov>
2022-12-07 15:14:13 -0800

    Add contributor and increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    author="R. C. Thomas, François Tessier",
+    author="R. C. Thomas, François Tessier, Narek Amirbekian",
     author_email="rcthomas@lbl.gov",
     data_files=[("share/jupyterhub/announcement/templates", ["templates/index.html"])],
     description="JupyterHub Announcement Service",
@@ -16,5 +16,5 @@ setup(
     name="jupyterhub-announcement",
     packages=["jupyterhub_announcement"],
     url="https://github.com/rcthomas/jupyterhub-announcement",
-    version="0.9.0.dev",
+    version="0.9.0",
 )
