1180646fc78d27e0dc9b2b58743bea11e93a01e9
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2025-05-20 09:51:54 +0200

    test: Add unit tests for handlers
    * Update CI workflow to bump actions versions and dependencies
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

---
+++ tests/conftest.py
@@ -0,0 +1,75 @@
+import time
+import pytest
+import socket
+import subprocess
+
+
+@pytest.fixture(scope="session")
+def jupyterhub_server(tmp_path_factory):
+    jupyterhub_dir = tmp_path_factory.mktemp("jupyterhub")
+    jupyterhub_config_file = jupyterhub_dir / "config.py"
+    config_content = """
+import os
+import sys
+
+CUSTOM_TEMPLATES_PATH = os.path.join(
+    sys.prefix, 'share', 'jupyterhub', 'announcement', 'templates'
+)
+
+c.Application.log_level = 10
+
+c.JupyterHub.services = [
+    {
+        'name': 'announcement',
+        'url': 'http://127.0.0.1:8888',
+        'command': ["python", "-m", "jupyterhub_announcement"],
+        'oauth_no_confirm': True,
+    }
+]
+
+c.JupyterHub.load_roles = [
+    {
+        "name": "user",
+        "scopes": ["access:services!service=announcement", "self"],
+    }
+]
+
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
+c.Authenticator.allow_all = True
+c.Authenticator.admin_users = ['admin']
+
+c.JupyterHub.template_paths = [CUSTOM_TEMPLATES_PATH]
+
+c.JupyterHub.spawner_class = 'jupyterhub.spawner.SimpleLocalProcessSpawner'
+"""
+    with open(jupyterhub_config_file, 'w') as f:
+        f.write(config_content)
+
+    proc = subprocess.Popen(
+        [
+            "jupyterhub",
+            "--config",
+            jupyterhub_config_file
+        ],
+        cwd=jupyterhub_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+    # Check it started successfully with a timeout of 60s
+    start = time.time()
+    while not is_server_up(8000) and time.time() - start < 60:
+        time.sleep(5)
+
+    yield proc
+
+    # Shut it down at the end of the pytest session
+    proc.terminate() 
+
+
+def is_server_up(port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(('localhost', port)) == 0
+    except:
+        return False
